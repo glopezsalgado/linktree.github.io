4e969d6332d20f985dc2b403095e2fb238dce68e
gsalgado <gsalgado@umich.edu>
2024-03-19 18:36:49 -0400

    links fixed

--- src/components/LinktreeSite.js
+++ src/components/LinktreeSite.js
@@ -11,9 +11,9 @@ export const LinktreeSite = () => {
     return (
         <div className="linktree-container">
             {/* Add your links here */}
-            <a href="ko-fi.com/yourcsgirlie" className="buttonStyle">Resume Template</a>
-            <a href="ko-fi.com/yourcsgirlie">Guide to Web Development</a>
-            <a href="ko-fi.com/yourcsgirlie">Buy Me a Coffee?</a>
+            <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Resume Template</a>
+            <a href="https://ko-fi.com/yourcsgirlie">Guide to Web Development</a>
+            <a href="https://ko-fi.com/yourcsgirlie">Buy Me a Coffee?</a>
         </div>
     )
 }

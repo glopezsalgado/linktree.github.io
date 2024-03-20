8fd71ce2c6cd42f4e1f4fe69674a0d25a4c620be
gsalgado <gsalgado@umich.edu>
2024-03-19 22:21:58 -0400

    added resume link

--- src/components/LinktreeSite.js
+++ src/components/LinktreeSite.js
@@ -14,7 +14,7 @@ export const LinktreeSite = () => {
         <div className="linktree-container">
             {/* Add your links here */}
             <img src={profilePic} className="myImage" />
-            <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Resume Template</a>
+            <a href="https://docs.google.com/document/d/13EHjqEDUue66Ka25F4C4WICOE0SYxOc08TkMjlt27nM/edit" className="buttonStyle">Resume Template</a>
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Guide to Web Development</a>
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Buy Me a Coffee?</a>
         </div>

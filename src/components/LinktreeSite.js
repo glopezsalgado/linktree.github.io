2210795a4faf23f085bafb05381ca1bceb3a129d
gsalgado <gsalgado@umich.edu>
2024-03-19 20:37:27 -0400

    adding image

--- src/components/LinktreeSite.js
+++ src/components/LinktreeSite.js
@@ -6,11 +6,14 @@ import {
 } from "react-router-dom";
 
 //
+import profilePic from '../assets/img/yourcsgirliepic.jpg';
+
 
 export const LinktreeSite = () => {
     return (
         <div className="linktree-container">
             {/* Add your links here */}
+            <img src={profilePic} className="myImage" />
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Resume Template</a>
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Guide to Web Development</a>
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Buy Me a Coffee?</a>

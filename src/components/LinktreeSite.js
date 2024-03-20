8cdfe135e03483cfbafa67e187a30cb10c1be21d
gsalgado <gsalgado@umich.edu>
2024-03-19 22:49:02 -0400

    added dark mode and center in profile

--- src/components/LinktreeSite.js
+++ src/components/LinktreeSite.js
@@ -14,6 +14,8 @@ export const LinktreeSite = () => {
         <div className="linktree-container">
             {/* Add your links here */}
             <img src={profilePic} className="myImage" />
+            <h1>@yourCSgirlie</h1>
+            <h2>iOS Mobile App Developer | Test Engineer | Web Developer | Researcher </h2>
             <a href="https://docs.google.com/document/d/13EHjqEDUue66Ka25F4C4WICOE0SYxOc08TkMjlt27nM/edit" className="buttonStyle">Resume Template</a>
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Guide to Web Development</a>
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Buy Me a Coffee?</a>

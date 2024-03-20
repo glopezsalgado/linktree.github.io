dbb64b6c9cdd660e0dd983fab2089934973688d9
gsalgado <gsalgado@umich.edu>
2024-03-19 23:46:06 -0400

    fixing IG headers and pic

--- src/components/LinktreeSite.js
+++ src/components/LinktreeSite.js
@@ -15,7 +15,7 @@ export const LinktreeSite = () => {
             {/* Add your links here */}
             <img src={profilePic} className="myImage" />
             <h1 className="IGat">@yourCSgirlie</h1>
-            <h2>iOS Mobile App Developer | Test Engineer | Web Developer | Researcher </h2>
+            <h2 className="myRoles">iOS Mobile App Developer | Test Engineer | Web Developer | Researcher </h2>
             <a href="https://docs.google.com/document/d/13EHjqEDUue66Ka25F4C4WICOE0SYxOc08TkMjlt27nM/edit" className="buttonStyle">Resume Template</a>
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Guide to Web Development</a>
             <a href="https://ko-fi.com/yourcsgirlie" className="buttonStyle">Buy Me a Coffee?</a>

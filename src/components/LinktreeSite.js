cb4909671c22488e9da6d2e7fbda86b2075ba53f
gsalgado <gsalgado@umich.edu>
2024-05-18 12:54:37 -0400

    changed profile pic

--- src/components/LinktreeSite.js
+++ src/components/LinktreeSite.js
@@ -13,7 +13,7 @@ export const LinktreeSite = () => {
     return (
         <div className="linktree-container">
             {/* Add your links here */}
-            <img src={profilePic} className="myImage" />
+            <img src={profilepic2} className="myImage" />
             <h1 className="IGat">@yourCSgirlie</h1>
             <h2 className="myRoles">iOS Mobile App Developer | Test Engineer | Web Developer | Researcher </h2>
             <a href="https://docs.google.com/document/d/13EHjqEDUue66Ka25F4C4WICOE0SYxOc08TkMjlt27nM/edit" className="buttonStyle">Resume Template</a>
